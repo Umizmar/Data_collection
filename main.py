62d778e64a9c193999c8116f61658e13856b639e
Marat Mizitov <maraentus@mail.ru>
2023-10-10 21:18:05 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+
+url = ("https://api-seller.ozon.ru/v1/description-category/tree")
+
+headers = {'Client-Id': '', 'Api-Key': ''}
+
+resp = requests.post(url, headers=headers).json()
+
+catalog = []
+for categories in resp.get('result'):
+    catalog.append(categories.get("category_name"))
+
+print('\n'.join(catalog))
+print()
+user_select = input('Выберите категорию из списка сверху - ')
+
+user_cat = []
+if user_select in catalog:
+    for select_categories in resp.get('result'):
+        if select_categories.get("category_name") == user_select:
+            for cat_list in select_categories.get('children'):
+                user_cat.append(cat_list.get("category_name"))
+else:
+    print()
+    print('Такого товара нет')
+
+print()
+print('\n'.join(user_cat))
