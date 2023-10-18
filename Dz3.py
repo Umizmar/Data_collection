76b13faa58f3d0d290eb29ff10ea241bd1aea097
Marat Mizitov <maraentus@mail.ru>
2023-10-18 23:14:00 +0300

    dz2

---
+++ Dz3.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+
+db = client['DZ3']
+collection = db.books
+
+# for doc in collection.find({"available":3}):
+#     print(doc)
+
+# for doc in collection.find({"available":{"$gt":19}}):
+#     print(doc)
+
+# for doc in collection.find({'$or':[{"available":{"$gt":20}},{"price":{"$gt":55}}]}):
+#      print(doc)
+
+# for doc in collection.find({'title':{'$regex':'Sl.'}},{'description':0}).sort('price'):
+#     print(doc)
+
+# collection.update_one({'title':'Thinking, Fast and Slow'}, {'$set':{'title':'Thinking'}})
